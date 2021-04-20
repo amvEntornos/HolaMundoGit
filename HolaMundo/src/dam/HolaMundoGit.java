de84f67cb87cc922d45ce4f53e1fd75709900db2
entornos <entornos@correo.es>
2021-04-20 20:54:40 +0200

    cab pie

--- HolaMundo/src/dam/HolaMundoGit.java
+++ HolaMundo/src/dam/HolaMundoGit.java
@@ -3,11 +3,13 @@ package dam;
 public class HolaMundoGit {
 
 	public static void main(String[] args) {
+		System.out.println("Start!");
 		int cont = 1;
 		while (cont <= 10) {
 			System.out.println("¡Hola mundo!");
 			cont++;
 		}
+		System.out.println("End!");
 
 	}
 
